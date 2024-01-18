5fcf8805b12236466445f85d997b7285ece779fe
Sushvin MariMuthu <sushvinmarimuthu@gmail.com>
2024-01-18 12:46:27 +0530

    middleware and package script Updated.

--- src/middleware.js
+++ src/middleware.js
@@ -6,13 +6,11 @@ export async function middleware(request) {
     const isPublicPath = path === '/auth/login' || path === '/auth/register' || path === '/auth/forgot-password' || path === '/auth/reset-password';
 
     const next_auth_session_token = request.cookies.get('next-auth.session-token')?.value || request.cookies.get('__Secure-next-auth.session-token')?.value || null;
-    console.log("next_auth_session_token - ", next_auth_session_token);
     if ((isPublicPath || path === '/') && next_auth_session_token) {
         const decoded = await decode({
             token: next_auth_session_token,
             secret: process.env.SECRET,
         });
-        console.log("D - ", decoded)
         return NextResponse.redirect(new URL(`/${decoded?._id}/home`, request.nextUrl))
     }
 
